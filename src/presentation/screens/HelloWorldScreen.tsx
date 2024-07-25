67c1479bfc124782bf2068fe2eb3ae4d0c50ea06
Emilio Gril <emiliogril@gmail.com>
2024-07-25 17:11:49 -0300

    cambio de nuevo

---
+++ src/presentation/screens/HelloWorldScreen.tsx
@@ -0,0 +1,27 @@
+
+import { View, Text, StyleSheet } from 'react-native'
+
+interface Props {
+    name?: String;
+}
+
+export const HelloWorldScreen = ( { name = ''} : Props) => {
+  return (
+    <View style={ styles.container }>
+        <Text style={ styles.title }>Hola Mundo { name }</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 45,
+        textAlign: 'center',
+        color: 'black',
+        padding: 20
+    }
+})
